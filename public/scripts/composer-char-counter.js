bbf74c58e988c6a86a578d35370d6322d7c11732
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-11-23 08:24:50 -0500

    Remove console log since character count is working

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     const counterElement = $('#tweet-character-limit');
     const remainingTextLength = 140 - value;
 
-    console.log(`Updating counter: ${remainingTextLength}`);
     counterElement.html(remainingTextLength);
     if (remainingTextLength < 0) {
       counterElement.addClass('error');
@@ -18,13 +17,11 @@ $(document).ready(function() {
 
   const element = getTweetInputElement()[0];
   if (element) {
-    console.log(element);
     updateCounter(element.value.length)
   }
   
 
   getTweetInputElement().on('input', function(event) {
-    console.log(event);
     const element = event.target;
     if (!element) {
       return;
