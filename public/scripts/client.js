2e14c3aa8854ae0f93f1528bb6f1ffec06a984ab
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-11-23 08:25:50 -0500

    Properly loading tweets and creating new one
    Using jQuery to load the tweets and upon creating one sending a AJAX request. Properly display the just sent tweet at the top. Add error handling before sending the tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,95 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const MAX_TWEET_LENGTH = 141;
 
+
+const loadTweets = function() {
+
+  $.get('/tweets',
+    (tweets) => {
+      renderTweets(tweets);
+    }
+  );
+}
+
+const renderTweets = function(tweets) {
+  let tweetContainer = $("#tweets");
+  tweetContainer.empty();
+  tweets.forEach(element => {
+    const tweet = createTweetElement(element);
+    tweetContainer.prepend(tweet);
+  });
+}
+
+const  createTweetElement = function(data) {
+  const dateLabel = timeago.format(data.created_at);
+  return `
+  <article class="tweet">
+  <header class="tweet__header">
+    <div class="header__user">
+
+      <img class="user__avatar" src="${data.user.avatars}">
+      <h2 class="user__name">
+        ${data.user.name}
+      </h2>
+    </div>
+    <h2 class="header_handle grey">
+      ${data.user.handle}
+    </h2>
+  </header>
+  <section class="tweet__body">
+    ${data.content.text}
+  </section>
+  <footer class="tweet__footer">
+    <p class="footer__date">
+      ${dateLabel}
+    </p>
+    <div class="footer__controls">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+</article>
+  `;
+}
+
+
+const clearErrorMessage = function() {
+  $('#errorMessage').remove();
+}
+
+const displayErrorMessage = function(error) {
+  $("#new-tweet__inputs").append(`<label id="errorMessage" class="error" for="text" id=errorMessage>${error}</label>`);
+};
+
+
+const sendTweet = function(form) {
+  clearErrorMessage();
+  const formData = form.serializeArray();
+  console.log(formData);
+  if(formData.length === 0 || formData[0].value.length === 0) {
+    displayErrorMessage("Missing content of tweet");
+    return;
+  }
+
+  if(formData[0].value.length >= MAX_TWEET_LENGTH) {
+    displayErrorMessage("Tweet is too long");
+    return;
+  }
+  console.log(form.serializeArray());
+  $.post("/tweets/", form.serializeArray(), (result) => {
+    loadTweets();
+  });
+}
+
+$(document).ready(function(form) {
+  loadTweets();
+
+  $('#new-tweet__form').on('submit', (event) => {
+    event.preventDefault();
+    const form = $(event.target);
+    sendTweet(form);
+  });
+});
